c14c60e3c6bfe047c6d5674c36991ccd98228571
Stephen Heywood <stephen@ii.coop>
2024-03-14 17:16:00 +1300

    feat: update build process to include requiured flatpaks

--- build-all.sh
+++ build-all.sh
@@ -6,25 +6,28 @@ set -o pipefail
 
 TIMESTAMP=$(date +'%Y%m%d-%H%M')
 cd fedora-kinoite
-docker build -t registry.msi.heyste.nz/kinoite:$TIMESTAMP -f Containerfile .
-docker push registry.msi.heyste.nz/kinoite:$TIMESTAMP
+podman build -t registry.msi.heyste.nz/kinoite:$TIMESTAMP -f Containerfile .
+podman push registry.msi.heyste.nz/kinoite:$TIMESTAMP
+podman images | grep $TIMESTAMP
 cd -
 
 ISO_TMPDIR=$(mktemp -d /tmp/iso-$TIMESTAMP-XXXX)
 echo $ISO_TMPDIR
 cd $ISO_TMPDIR
 
-docker images | grep $TIMESTAMP
+FLATPAK_SONIC_PI="app/net.sonic_pi.SonicPi/x86_64/stable runtime/org.kde.Platform/x86_64/5.15-23.08"
+FLATPAK_ECLIPSE="app/org.eclipse.Java/x86_64/stable runtime/org.gnome.Platform/x86_64/45"
 
-docker run --rm --privileged \
-           --volume .:/isogenerator/output \
-           --add-host registry.msi.heyste.nz:192.168.111.202 \
-           -e VERSION=39 \
-           -e IMAGE_REPO=registry.msi.heyste.nz \
-           -e IMAGE_NAME=kinoite \
-           -e IMAGE_TAG=$TIMESTAMP \
-           -e VARIANT=Kinoite \
-           ghcr.io/ublue-os/isogenerator:39
+sudo podman run --rm --privileged \
+                --volume .:/github/workspace/build \
+		docker.io/heyste/build-container-installer:latest \
+		VERSION=39  \
+		IMAGE_REPO=registry.msi.heyste.nz \
+		IMAGE_NAME=kinoite \
+		IMAGE_TAG=$TIMESTAMP \
+		VARIANT=Server \
+		FLATPAK_REMOTE_REFS="$FLATPAK_ECLIPSE $FLATPAK_SONIC_PI" \
+		FLATPAK_REMOTE_URL="https://flathub.org/repo/flathub.flatpakrepo"
 
 echo $ISO_TMPDIR
 ls -lh $ISO_TMPDIR
